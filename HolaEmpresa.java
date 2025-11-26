3465b01a7cdaf06858d8d880b283511bd6fc97eb
Vicoszzz-Calva <vcalvag2400@alumno.ipn.mx>
2025-11-25 20:39:37 -0600

    segunda versión

--- HolaEmpresa.java
+++ HolaEmpresa.java
@@ -1,5 +1,7 @@
-    public class HolaEmpresa {
-        public static void main(String[] args) {
+    public class HolaEmpresa
+    {
+        public static void main(String[] args)
+        {
             System.out.println("Hola Empresa");
         }
     }
